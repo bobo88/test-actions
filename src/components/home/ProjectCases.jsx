fd467e4372f7392f76ca809e5a5487c17389244b
bobo88 <yuanboi88@163.com>
2022-09-22 22:28:59 +0800

    perf: 首页新增链接

--- src/components/home/ProjectCases.jsx
+++ src/components/home/ProjectCases.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import { Card, Divider, Col, Row, } from 'antd';
 
 // https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png
-import  RollupImg from '@/assets/image/cases/rollup.png'
-import  NodeImg from '@/assets/image/cases/node.png'
-import  ReactImg from '@/assets/image/cases/react.png'
-import  HtmlImg from '@/assets/image/cases/html.png'
-import  NuxtImg from '@/assets/image/cases/nuxt.png'
-import  VueImg from '@/assets/image/cases/vue.png'
-import  VueTsImg from '@/assets/image/cases/vue_ts.jpg'
-import  EchartsImg from '@/assets/image/cases/echarts.jpg'
+import RollupImg from '@/assets/image/cases/rollup.png'
+import NodeImg from '@/assets/image/cases/node.png'
+import ReactImg from '@/assets/image/cases/react.png'
+import HtmlImg from '@/assets/image/cases/html.png'
+import NuxtImg from '@/assets/image/cases/nuxt.png'
+import VueImg from '@/assets/image/cases/vue.png'
+import VueTsImg from '@/assets/image/cases/vue_ts.jpg'
+import EchartsImg from '@/assets/image/cases/echarts.jpg'
 
 const { Meta } = Card;
 
@@ -17,18 +17,20 @@ const Skills = () => {
   return (
     <div className="project-cases-box mb20">
       <Divider orientation="left">Project cases</Divider>
-      
+
       <Row gutter={16} justify="space-around">
         <Col className="gutter-row" xs={12} sm={12} md={8} lg={6} xl={6}>
-          <div className="card-item mb20">
-            <Card
-              hoverable
-              style={{ width: 240 }}
-              cover={<img alt="example" className="case-item-img" src={VueTsImg} />}
-            >
-              <Meta title="Vue3/vite/TS" description="https://github.com/bobo88" />
-            </Card>
-          </div>
+          <a href="https://github.com/bobo88/project-basis/tree/main/vue3-multi-page" target='_blank' rel="noreferrer">
+            <div className="card-item mb20">
+              <Card
+                hoverable
+                style={{ width: 240 }}
+                cover={<img alt="example" className="case-item-img" src={VueTsImg} />}
+              >
+                <Meta title="Vue3" description="Vue3多页面配置项目" />
+              </Card>
+            </div>
+          </a>
         </Col>
         <Col className="gutter-row" xs={12} sm={12} md={8} lg={6} xl={6}>
           <a href="https://github.com/bobo88/test-actions" target='_blank' rel="noreferrer">
