c3bc11fa152ebb5307a20bfecd2835380284ced2
bobo88 <yuanboi88@163.com>
2022-09-22 14:35:54 +0800

    'feat:编辑about页面'

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,21 +1,23 @@
-import React, { useState } from 'react';
-import { Button } from 'antd';
+import React from 'react';
+import { Timeline } from 'antd';
 import './about.scss'
 
-const App = function () {
-    const [msg] = useState('我是msg信息688');
-    const [isHot, setIsHot] = useState(false);
+const About = function () {
 
     return (
         <div className='main-wrap about-box'>
-            <h1>About page</h1>
-            <p className={ isHot ? 'red' : 'green' }>{ msg }</p>
-            <p className='desc'>今天的天气真 <span className={ isHot ? 'red' : 'green' }>{ isHot ? '炎热' : '凉爽'}</span></p>
-            <button onClick={ () => setIsHot(!isHot) } style={{ margin: '0 20px 0 0' }}>changeState</button>
-            <input type="text" style={{marginRight: '20px'}} />
-            <Button className='mr20' type="primary">Button 6</Button>
-            <Button type="primary">Button 8</Button>
+            <Timeline>
+                <Timeline.Item>近10年的前端开发工作经验，追求“凡事必有回响”；</Timeline.Item>
+                <Timeline.Item>精通主流的前端技术栈：VUE全家桶、React全家桶、Typescript、Nuxt、Sass、Node/Express/Koa等</Timeline.Item>
+                <Timeline.Item>精通主流的前端自动化打包工具和流程：Gulp、Webpack、Rollup、Vite、GitActions、Nginx等</Timeline.Item>
+                <Timeline.Item>熟悉前端主流的UI组件库：ElementUI、Antd、Vant、Echarts等</Timeline.Item>
+                <Timeline.Item>有PHP、Node/Express/Koa、React等多种方式搭建博客网站的经验，涉及MySQL、pm2、Nginx等技术</Timeline.Item>
+                <Timeline.Item>喜爱搭建脚手架简化工作流程，喜爱输出技术文档思维导图（x-mind的狂热爱好者）</Timeline.Item>
+                <Timeline.Item>热衷技术能力“充电”，爱捣鼓</Timeline.Item>
+                <Timeline.Item>喜爱自驾游</Timeline.Item>
+                <Timeline.Item>Talk is cheap, show me the code!</Timeline.Item>
+            </Timeline>
         </div>
     )
 }
-export default App;
+export default About;
